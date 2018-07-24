19dcea6be141dd2eee7f19ca0e5e00a61e3a6f29
Mike Good <mgood@axiomatics.com>
2018-07-24 18:17:54 -0400

    Removed validation annotations
    Registration process is out of scope for this tutorial

--- src/main/java/com/axiomatics/model/User.java
+++ src/main/java/com/axiomatics/model/User.java
@@ -12,9 +12,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import org.hibernate.validator.constraints.Length;
 import org.springframework.data.annotation.Transient;
 
 @Entity
@@ -26,19 +23,13 @@ public class User {
 	@Column(name = "user_id")
 	private int id;
 	@Column(name = "email")
-	@Email(message = "*Please provide a valid Email")
-	@NotEmpty(message = "*Please provide an email")
 	private String email;
 	@Column(name = "password")
-	@Length(min = 5, message = "*Your password must have at least 5 characters")
-	@NotEmpty(message = "*Please provide your password")
 	@Transient
 	private String password;
 	@Column(name = "name")
-	@NotEmpty(message = "*Please provide your name")
 	private String name;
 	@Column(name = "last_name")
-	@NotEmpty(message = "*Please provide your last name")
 	private String lastName;
 	@Column(name = "active")
 	private int active;
